54a5e3dcfb2a0e93f17c4f92ef0527c88d23af11
OlegOKovalyov <koa2003@ukr.net>
2021-05-11 23:24:15 +0300

    1.0: https://trello.com/c/sV07SXTP/ Add new links

--- templates/support.php
+++ templates/support.php
@@ -7,4 +7,16 @@
 
     <p>Також ваш приємний відгук про плагін підбадьорить нас в щоденній праці :)</p>
 
+    <p>Залишити відгук на wp.org:
+        <a href="https://wordpress.org/plugins/ua-marketplace/#reviews">
+            https://wordpress.org/plugins/ua-marketplace/#reviews
+        </a>
+    </p>
+
+    <p>Залишити відгук на fb-сторінці:
+        <a href="https://www.facebook.com/morkvasite/reviews">
+            https://www.facebook.com/morkvasite/reviews
+        </a>
+    </p>
+
 <?php require_once( 'dashboard-footer.php' ); ?>
